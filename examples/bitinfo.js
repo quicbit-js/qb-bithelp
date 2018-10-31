f0afdfcd576d5800fb4e6b235657c9bc274a1690
mvoss9000 <mvoss9000@yahoo.com>
2018-10-31 12:04:15 -0400

    add Silicon Valley example

--- examples/bitinfo.js
+++ examples/bitinfo.js
@@ -4,3 +4,12 @@ bhelp.bitinfo('abcé', 'simple string')
 bhelp.bitinfo('\u00E6', 'a ligature')
 bhelp.bitinfo([ 0x10009, 0x10010 ], 'large (4-byte) characters')
 bhelp.bitinfo('做法方便上手。', 'some chinese')
+bhelp.bitinfo([
+    0b01000010,
+    0b01101001,
+    0b01110100,
+    0b01100011,
+    0b01101111,
+    0b01101001,
+    0b01101110
+], "Erlich's Shirt in Silicon Valley")
